fcb68a446f6b6ae3cf7dd1fd1afbc6ce8686b1f3
Igor Napierala <igor.napierala@cognifide.com>
2021-01-04 10:26:03 +0100

    format with data and user

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,22 +11,27 @@ const io = socketio(server);
 //Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+const botName = "ChatCord Bot";
+
 //Run when client connects
 io.on("connection", (socket) => {
   //Single client emit
-  socket.emit("message", "Welcome to Chatcord!");
+  socket.emit("message", formatMessage(botName, "Welcome to Chatcord!"));
 
   //Broadcast when a user connects - everybody except the user
-  socket.broadcast.emit("message", "A user has joined the chat");
+  socket.broadcast.emit(
+    "message",
+    formatMessage(botName, "A user has joined the chat")
+  );
 
   // Runs when client disconnects
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left the chat");
+    io.emit("message", formatMessage(botName, "A user has left the chat"));
   });
 
   // Listen for chatMessage
   socket.on("chatMessage", (msg) => {
-    io.emit("message", msg);
+    io.emit("message", formatMessage("USER", msg));
   });
 });
 
